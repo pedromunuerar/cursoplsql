3aa7617f8438ae621d91428ccbcd0a286fecad5c
pedromunuerar <pedromunuerar@gmail.com>
2025-11-04 21:35:16 +0100

    Update sesion1-json.sql

--- sesion1-json.sql
+++ sesion1-json.sql
@@ -9,6 +9,14 @@ INSERT INTO pedidos (cliente, datos)
 VALUES ('Carlos', '{"items":[{"producto":"Cámara","precio":250.0}],"envio":"24h"}');
 INSERT INTO pedidos (cliente, datos)
 VALUES ('Carlos', '{"items":[{"producto":"Perfume","precio":50.0}],"envio":"24h"}');
+INSERT INTO pedidos (cliente, datos)
+VALUES ('Carlos', '{"items":[{"producto":"Cámara","precio":250.0}],"envio":"24h"}');
+INSERT INTO pedidos (cliente, datos)
+VALUES ('Carlos', '{"items":[{"producto":"Perfume","precio":50.0}],"envio":"24h"}');
+INSERT INTO pedidos (cliente, datos)
+VALUES ('Carlos', '{"items":[{"producto":"Cámara","precio":250.0}],"envio":"24h"}');
+INSERT INTO pedidos (cliente, datos)
+VALUES ('Carlos', '{"items":[{"producto":"Perfume","precio":50.0}],"envio":"24h"}');
 
 
 SELECT
@@ -16,3 +24,16 @@ SELECT
    JSON_VALUE(datos, '$.items[0].precio') AS precio
 FROM pedidos
 where JSON_VALUE(datos, '$.items[0].precio')=50;
+
+--Si no tiene precio, se trata como null
+INSERT INTO pedidos (cliente, datos)
+VALUES ('Carlos', '{"items":[{"producto":"Cámara"}],"envio":"24h"}');
+INSERT INTO pedidos (cliente, datos)
+VALUES ('Carlos', '{"items":[{"producto":"Perfume"}],"envio":"24h"}');
+
+--Si no existe tambien
+SELECT
+   JSON_VALUE(datos, '$.items[9].producto') AS producto,
+   JSON_VALUE(datos, '$.items[0].precio') AS precio
+FROM pedidos
+where JSON_VALUE(datos, '$.items[0].precio') is null;

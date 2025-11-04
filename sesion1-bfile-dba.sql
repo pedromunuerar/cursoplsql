b0d6348daab5b6851cb34e2692e5c3b963067d6a
pedromunuerar <pedromunuerar@gmail.com>
2025-11-04 11:33:48 +0100

    Add comments for directory creation and permissions
    Added comments about directory permissions and requirements.

--- sesion1-bfile-dba.sql
+++ sesion1-bfile-dba.sql
@@ -1,6 +1,8 @@
 -- Como SYS, crear directorio virtual
 CREATE OR REPLACE DIRECTORY ARCHIVOS_DIR AS '/opt/oracle/archivos';
 
+--El directorio tiene que existir y tener permisos en él
+
 -- Dar permisos al usuario
 GRANT READ, WRITE  ON DIRECTORY ARCHIVOS_DIR TO C##DEVUSER1;
 GRANT READ, WRITE  ON DIRECTORY ARCHIVOS_DIR TO C##DEVUSER2;

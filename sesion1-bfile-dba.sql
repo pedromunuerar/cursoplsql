ddbb4a49c8a7a86158b27abea91af5557e2b5e31
pedromunuerar <pedromunuerar@gmail.com>
2025-11-04 11:50:03 +0100

    Grant permissions on DATA_PUMP_DIR to users

--- sesion1-bfile-dba.sql
+++ sesion1-bfile-dba.sql
@@ -6,3 +6,11 @@ CREATE OR REPLACE DIRECTORY ARCHIVOS_DIR AS '/opt/oracle/archivos';
 -- Dar permisos al usuario
 GRANT READ, WRITE  ON DIRECTORY ARCHIVOS_DIR TO C##DEVUSER1;
 GRANT READ, WRITE  ON DIRECTORY ARCHIVOS_DIR TO C##DEVUSER2;
+
+--Como tenemos problemas con directorios creados a mano por toda la dockerización usamos el DATA_PUMP_DIR
+
+GRANT READ, WRITE  ON DIRECTORY DATA_PUMP_DIR TO C##DEVUSER1;
+GRANT READ, WRITE  ON DIRECTORY DATA_PUMP_DIR TO C##DEVUSER2;
+
+SELECT directory_name, directory_path FROM all_directories;
+

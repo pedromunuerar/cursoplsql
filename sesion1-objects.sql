b0d87b8338fd8b94b541b70e4c0cb45c96dedcf0
pedromunuerar <pedromunuerar@gmail.com>
2025-11-04 23:53:11 +0100

    Update sesion1-objects.sql

--- sesion1-objects.sql
+++ sesion1-objects.sql
@@ -18,3 +18,31 @@ INSERT INTO clientes VALUES (
 /
 SELECT * FROM clientes c;
 SELECT c.domicilio.ciudad FROM clientes c;
+
+/
+--Tablas anidadas y colecciones
+   
+CREATE TYPE telefono_t AS OBJECT (
+   tipo VARCHAR2(10),
+   numero VARCHAR2(20)
+);
+
+CREATE TYPE lista_telefonos_t AS TABLE OF telefono_t;
+
+CREATE TABLE personas (
+   id NUMBER PRIMARY KEY,
+   nombre VARCHAR2(100),
+   telefonos lista_telefonos_t
+) NESTED TABLE telefonos STORE AS telefonos_nt;
+
+INSERT INTO personas VALUES (
+   1,
+   'Juan López',
+   lista_telefonos_t(telefono_t('Móvil','600123123'), telefono_t('Fijo','911223344'))
+);
+
+
+sql
+Copiar código
+SELECT p.nombre, t.numero
+FROM personas p, TABLE(p.telefonos) t;
